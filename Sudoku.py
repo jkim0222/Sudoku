817c6d418f65f4eb4ec04fd888771ff077cd3a96
jkim0222 <jkim0222@users.noreply.github.com>
2017-06-08 10:02:32 +0900

    요기까지만?
    아이들에게 요기까지만 일단 좀 봐 볼래?

--- Sudoku.py
+++ Sudoku.py
@@ -169,6 +169,73 @@ def find_hidden_singles(s, debug):
         print("Found", found, "hidden single(s).")
     return found
 
+def are_there_the_same_naked_pairs(s, y, x, g_type, g_index):
+    if (len(cands(s[y][x])) == 2):
+        print("found")
+    return sy, sx
+
+"""
+그룹별로 루프를 돌릴 수 있도록 하는데 - 요 루틴도 함수로 따로 떼어 내야 할 듯~
+일단은 naked만 찾으면 되니까 ...
+혹시 pair가 있으면 그 pair와 동일한 페어가 동일한 그룹에 들어 있는지를 보고,
+찾으면 그 그룹 안에 있는 그 두 개의 candidates를 제거하는 루틴을 만든다.
+
+pseudo 루틴으로 ...
+pair가 있으면 같은 pair가 있는를 검사해 주는 함수를 만들고,
+그리고 나면 그 그룹에서 그 pair의 candidates를 제거해 주는 루틴을 만들어서 전달?
+"""
+
+def find_naked_pairs(s, debug):
+    found = 0
+    for y in range(9):
+        n_cands = [0] * 10      # 10 for readability
+        hidden_in = [0] * 10
+        for x in range(9):
+            if s[y][x] == 0:
+                for i in range(len(cand[y][x])):
+                    n_cands[cand[y][x][i]] += 1
+                    hidden_in[cand[y][x][i]] = x
+        for i in range(1, 10):
+            if n_cands[i] == 1:
+                s[y][hidden_in[i]] = i
+                if debug > 1:
+                    print("Hidden single", i, "on row", y, "x = ", hidden_in[i])
+                found += 1
+    for x in range(9):
+        n_cands = [0] * 10      # 10 for readability
+        hidden_in = [0] * 10
+        for y in range(9):
+            if s[y][x] == 0:
+                for i in range(len(cand[y][x])):
+                    n_cands[cand[y][x][i]] += 1
+                    hidden_in[cand[y][x][i]] = y
+        for i in range(1, 10):
+            if n_cands[i] == 1:
+                s[hidden_in[i]][x] = i
+                if debug > 1:
+                    print("Hidden single", i, "on column", x, "y = ", hidden_in[i])
+                found += 1
+    for i in range(9):
+        n_cands = [0] * 10      # 10 for readability
+        hidden_in_y = [0] * 10
+        hidden_in_x = [0] * 10
+        for y in range((i // 3) * 3, (i // 3) * 3 + 3):
+            for x in range((i % 3) * 3, (i % 3) * 3 + 3):
+                if s[y][x] == 0:
+                    for j in range(len(cand[y][x])):
+                        n_cands[cand[y][x][j]] += 1
+                        hidden_in_y[cand[y][x][j]] = y
+                        hidden_in_x[cand[y][x][j]] = x
+        for j in range(1, 10):
+            if n_cands[j] == 1:
+                s[hidden_in_y[j]][hidden_in_x[j]] = j
+                if debug > 1:
+                    print("Hidden single", j, "in block", i, "on", hidden_in_y[j], "x", hidden_in_x[j])
+                found += 1
+    if debug:
+        print("Found", found, "hidden single(s).")
+    return found
+
 def main(n, debug):
     s = sudokus[n]
     f = show_sudoku(s)
@@ -187,4 +254,4 @@ def main(n, debug):
             print(str(81-f), "more to go.")
     show_sudoku(s)
 
-main(1, 2)
+main(2, 2)
